514f7e928ebd6fb2706bd22297e6a13ecc43c77a
Raghav Pandit <panditraghav@gmail.com>
2025-05-28 19:13:09 +0530

    added kokoro for on device tts

--- Lingo-Backend/ai/api/routes/ai_routes.py
+++ Lingo-Backend/ai/api/routes/ai_routes.py
@@ -1,6 +1,6 @@
 import asyncio
 from asyncio import Queue
-from fastapi import APIRouter, Response, status
+from fastapi import APIRouter, Response, HTTPException, status
 from google import genai
 from uuid import uuid4
 from google.genai.chats import AsyncChat
@@ -8,8 +8,10 @@ from google.genai.types import GenerateContentConfig
 from pydantic import BaseModel
 import os
 
+from utils.files import delete_files
 from utils.text_to_speech import generate_audio
 from utils.audio import combine_wav
+from utils.tts_kokoro import kokoro_generate_audio
 
 ai_router = APIRouter()
 
@@ -39,7 +41,7 @@ async def audio_consumer(queue: Queue[str], file_paths: list[str]):
             file_paths.append(path)
         queue.task_done()
 
-default_chat = client.aio.chats.create(model='gemini-2.0-flash', config = GenerateContentConfig(system_instruction= "You are a AI english tutor of Lingo English Learning application, you have to explain all the doubts that the user have. You have to generate text like you are talking to a person, so the output text should have natural flow. Instead of it being formatted for reading, it have to be generated for listening. Don't output text in markdown format, just plain text."))
+default_chat = client.aio.chats.create(model='gemini-2.0-flash', config = GenerateContentConfig(system_instruction= "You are a AI english tutor of Lingo English Learning application, you have to explain all the doubts that the user have. You have to generate text which will be input to a text-to-speech model, so you have to generate text which doesn't have formattings like bullet points, headings, etc. Don't output text in markdown format, just plain text."))
 
 @ai_router.post("/chat/{chat_id}", status_code= status.HTTP_200_OK)
 async def post_chat_message(chat_id: str, message: ChatMessage, response: Response):
@@ -56,9 +58,7 @@ async def post_chat_message(chat_id: str, message: ChatMessage, response: Respon
     file_paths: list[str] = []
     consumers = []
 
-    for i in range(3):
-        consumers.append(asyncio.create_task(audio_consumer(queue, file_paths)))
-        consumers.append(asyncio.create_task(audio_consumer(queue, file_paths)))
+    for _ in range(3):
         consumers.append(asyncio.create_task(audio_consumer(queue, file_paths)))
 
     print("Consumer created!")
@@ -86,7 +86,8 @@ async def post_chat_message(chat_id: str, message: ChatMessage, response: Respon
             await queue.put(sentence)
             sentence = chunk.text[full_stop_index + 2: -1]
 
-    await queue.put(sentence)
+    if sentence_send_for_audio_gen_len < len(full_text) and sentence.strip() != "":
+        await queue.put(sentence)
     await queue.join()
     print(file_paths)
 
@@ -100,3 +101,85 @@ async def post_chat_message(chat_id: str, message: ChatMessage, response: Respon
 @ai_router.get("/chat/{chat_id}")
 def get_chat(chat_id: str):
     return {chat_id: chats_dict.get(chat_id)}
+
+
+###########################################
+# KOKORO
+###########################################
+
+
+async def kokoro_tts_consumer(queue: Queue[str], file_paths: list[str]):
+    while True:
+        item = await queue.get()  # Async get
+        print("(KOKORO) Got queue item: ", item, sep= "")
+        path = kokoro_generate_audio(item)
+        print("(KOKORO) Audio generated for: ", item)
+        print("(KOKORO) Path is: ", path)
+        if path != "":
+            file_paths.append(path)
+        queue.task_done()
+
+@ai_router.post("/chat/kokoro/{chat_id}", status_code= status.HTTP_200_OK)
+async def kokoro_post_chat(chat_id: str, message: ChatMessage, response: Response):
+    queue = asyncio.Queue[str]()
+    file_paths: list[str] = []
+    consumers = []
+
+    for _ in range(3):
+        consumers.append(asyncio.create_task(kokoro_tts_consumer(queue, file_paths)))
+    try: 
+        print("Chat message: ", message)
+        current_chat = default_chat
+
+        if current_chat == None:
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return {"message": "Chat not found!, please create new chat"}
+
+        sentence = ""
+        full_text = ""
+        sentence_send_for_audio_gen_len = 0
+
+        async for chunk in await current_chat.send_message_stream(message.text):
+            print("ChunkText: ", chunk.text, end = " ")
+            if chunk.text == None:
+                continue
+            full_text += chunk.text
+
+            full_stop_index = chunk.text.find(".", 0, -1)
+            print("full_stop_index: ", full_stop_index)
+
+            if full_stop_index == -1:
+                sentence += chunk.text
+            else:
+                sentence += chunk.text[0:full_stop_index + 1]
+                # full_stop_list.append(curr)
+                print("Putting sentence into queue: ", sentence, sep ="")
+                sentence_send_for_audio_gen_len += len(sentence)
+                await queue.put(sentence)
+                sentence = chunk.text[full_stop_index + 2: -1]
+
+        if sentence_send_for_audio_gen_len < len(full_text) and sentence.strip() != "":
+            await queue.put(sentence)
+        await queue.join()
+        print(file_paths)
+
+        for consumer in consumers:
+            consumer.cancel()
+
+        final_audio_file = combine_wav(file_paths)
+        delete_files(file_paths)
+
+        return {"text": full_text, "final_audio_file": final_audio_file}
+
+    except asyncio.CancelledError:
+        print("Request cancelled!")
+        for consumer in consumers:
+            consumer.cancel()
+        return {"message": "Request cancelled!"}
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        for consumer in consumers:
+            consumer.cancel()
+        raise HTTPException(status_code=500, detail="Internal server error")
+
--- Lingo-Backend/ai/utils/audio.py
+++ Lingo-Backend/ai/utils/audio.py
@@ -1,5 +1,8 @@
 from pydub import AudioSegment
 from uuid import uuid4
+import os
+
+tts_folder_path = os.environ.get("TTS_FOLDER_PATH") or "."
 
 def combine_wav(file_list: list[str]) -> str:
     combined = AudioSegment.from_wav(file_list[0])
@@ -7,6 +10,8 @@ def combine_wav(file_list: list[str]) -> str:
         audio = AudioSegment.from_wav(file)
         combined += audio
 
-    file_name = f"{uuid4()}.wav"
+    id = uuid4().__str__().split('-')[0]
+    file_name = f"{tts_folder_path}/{id}.wav"
+    
     combined.export(file_name, format="wav")
     return file_name
