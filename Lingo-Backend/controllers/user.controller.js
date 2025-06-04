e0f15342c11f00348bb56c645a2d455b90c8fc75
Raghav Pandit <panditraghav@gmail.com>
2025-06-04 17:18:55 +0530

    Now accepting bearer token for isAuthenticated middleware

--- Lingo-Backend/controllers/user.controller.js
+++ Lingo-Backend/controllers/user.controller.js
@@ -122,6 +122,7 @@ export const login = async (req, res) => {
         return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSit: 'strict' }).json({
             message: `Welcome back ${user.fullName}`,
             user,
+            token,
             success: true
         })
 
